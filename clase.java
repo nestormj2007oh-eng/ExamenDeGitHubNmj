2177993dd6febb2bbdca2a7d2e9cde77047b02b4
Nestor <nestormj2007oh@gmail.com>
2026-02-12 12:35:09 +0100

    Cambios.v2

--- clase.java
+++ clase.java
@@ -1,11 +1,19 @@
-public static void multiplos(int numeroMax) {
-    for(int i=0;i<numeroMax;i++) {
-        if(i%49==0){
-            System.out.println("Múltiplo de 49: "+i);
+
+public class clase {
+    public static void main(String[] args) {
+        System.out.println("hola Mundo"); 
         }
-        else{
-            System.out.println("Multiplo de 100: "+i);
+
+    public static void multiplos(int numeroMax) {
+        for(int i=0;i<numeroMax;i++) {
+            if(i%49==0){
+                System.out.println("Múltiplo de 49: "+i);
+            }
+            else{
+                System.out.println("Multiplo de 100: "+i);
+            }
+            
         }
-        
     }
+
 }
