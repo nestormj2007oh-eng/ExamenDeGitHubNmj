758fcc77801022f5166dc5835ba11e5d1e39b969
Nestor <nestormj2007oh@gmail.com>
2026-02-12 12:27:52 +0100

    Cambios.v1

---
+++ clase.java
@@ -0,0 +1,11 @@
+public static void multiplos(int numeroMax) {
+    for(int i=0;i<numeroMax;i++) {
+        if(i%49==0){
+            System.out.println("Múltiplo de 49: "+i);
+        }
+        else{
+            System.out.println("Multiplo de 100: "+i);
+        }
+        
+    }
+}
