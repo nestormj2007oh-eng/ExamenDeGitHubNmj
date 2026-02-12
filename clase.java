7adf7dcbe7d91247847693c265a3a381d54526f6
Nestor <nestormj2007oh@gmail.com>
2026-02-12 12:37:48 +0100

    cambios.v3

--- clase.java
+++ clase.java
@@ -6,6 +6,9 @@ public class clase {
 
     public static void multiplos(int numeroMax) {
         for(int i=0;i<numeroMax;i++) {
+            if(i%7==0){
+                System.out.println("Múltiplo de 7: "+i);
+            }
             if(i%49==0){
                 System.out.println("Múltiplo de 49: "+i);
             }
